623e3d31403df37122649efcc1a33bc6a58a2677
seheya <sv.voitsekhovska@gmail.com>
2023-08-26 22:24:04 +0100

    123

---
+++ story_content/user.js
@@ -0,0 +1,55 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6FQXAqwGnpf":
+        Script1();
+        break;
+      case "5dgHidt9Vsn":
+        Script2();
+        break;
+      case "5pOJIk4Fw4S":
+        Script3();
+        break;
+      case "669hpRBjqqi":
+        Script4();
+        break;
+      case "6mU8WuVj5Nh":
+        Script5();
+        break;
+      case "5lDXgjBiZm2":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.body.style.background = "#ffffff";
+}
+
+function Script2()
+{
+  document.body.style.background = "#ffffff";
+}
+
+function Script3()
+{
+  document.body.style.background = "#32046A";
+}
+
+function Script4()
+{
+  document.body.style.background = "#404040";
+}
+
+function Script5()
+{
+  document.body.style.background = "#404040";
+}
+
+function Script6()
+{
+  document.body.style.background = "#32046A";
+}
+
